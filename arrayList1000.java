9095560f3fe7ea43c61eaec6dc3609118288e221
himanshu <himanshu.pratap@Gspann.com>
2015-10-27 15:18:51 +0530

    added 1000ArrayList

---
+++ arrayList1000.java
@@ -0,0 +1,30 @@
+package oops;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class arrayList1000 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		ArrayList<Integer> objarrList =  new ArrayList<Integer>();
+		long startTime = System.currentTimeMillis();
+		System.out.println("startTime of loop:" + startTime);
+		long endTime = 0;
+		for(int i=0; i<1000;i++)
+		{
+		   objarrList.add(num++);	
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println("endTime of loop:" + endTime);
+		long timeneeded =  endTime - startTime;
+		System.out.println("time to execute loop:" + timeneeded);
+		Iterator<Integer> itr = objarrList.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
